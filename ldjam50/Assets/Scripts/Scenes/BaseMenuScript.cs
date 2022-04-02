82d896e5e6c222005f6390c068d2ce0af69ad1ad
XpArKeR <xparker@gmx.net>
2022-04-02 14:05:48 +0200

    Updated Credits and SavedGamesScenes

---
+++ ldjam50/Assets/Scripts/Scenes/BaseMenuScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Assets.Scripts.Base;
+
+namespace Assets.Scripts.Scenes
+{
+    public class BaseMenuScript : MonoBehaviour
+    {
+        public void Back()
+        {
+            Base.Core.Game.ChangeScene(SceneNames.MainMenu);
+        }
+    }
+}
