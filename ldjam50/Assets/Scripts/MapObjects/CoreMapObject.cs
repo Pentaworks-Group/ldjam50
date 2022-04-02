157c3c9a4c12c1c8b759a7be88576b2073b317ed
Rentem <salasam@gmx.net>
2022-04-02 10:08:57 +0200

    MapObjects

---
+++ ldjam50/Assets/Scripts/MapObjects/CoreMapObject.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoreMapObject
+{
+    public String Image { get; set; }
+    public Vector2 Location { get; set; }
+    public String Name { get; set; }
+
+}
---
+++ ldjam50/Assets/Scripts/MapObjects/CoreUnits.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreUnits 
+{
+    public float MaxSpeed { get; set; }
+    public float Grwoth { get; set; }
+    public Vector2 Target { get; set; }
+}
