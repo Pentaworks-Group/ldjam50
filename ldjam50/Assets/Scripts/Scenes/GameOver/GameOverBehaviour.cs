0229047b702edccd6d4113abb251d38f26a118c8
XpArKeR <xparker@gmx.net>
2022-04-03 12:43:55 +0200

    Updated GameOver scene

--- ldjam50/Assets/Scripts/Scenes/GameOver/GameOverBehaviour.cs
+++ ldjam50/Assets/Scripts/Scenes/GameOver/GameOverBehaviour.cs
@@ -1,10 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
+
 using Assets.Scripts.Base;
+
+using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverBehaviour : MonoBehaviour
 {
+    public Text ElapesedTimeText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +16,8 @@ public class GameOverBehaviour : MonoBehaviour
         Core.Game.BackgroundAudioManager.Clips = Core.Game.AudioClipListTransition;
         Core.Game.BackgroundAudioManager.Resume();
         Core.Game.BackgroundAudioManager.Clips = Core.Game.AudioClipListMenu;
+
+        this.ElapesedTimeText.text = String.Format("You managed to stay in power for {0:F1} seconds.", Core.Game.State.ElapsedTime);
     }
 
     // Update is called once per frame
@@ -22,6 +28,8 @@ public class GameOverBehaviour : MonoBehaviour
 
     public void Back()
     {
+        Assets.Scripts.Base.Core.Game.Stop();
+
         Core.Game.ChangeScene(SceneNames.MainMenu);
     }
 
