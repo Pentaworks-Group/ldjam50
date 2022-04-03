613174220603097b14f899f453243fb8de696801
XpArKeR <xparker@gmx.net>
2022-04-03 21:06:01 +0200

    Initial Shop

--- ldjam50/Assets/Scripts/MapObjects/Defaults/TroopDefault.cs
+++ ldjam50/Assets/Scripts/MapObjects/Defaults/TroopDefault.cs
@@ -1,10 +1,9 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class TroopDefault
 {
+    public String Type { get; set; }
     public List<String> Names { get; set; }
     public float MaxSpeed { get; set; }
     public float Health { get; set; }
@@ -12,7 +11,5 @@ public class TroopDefault
     public float Strength { get; set; }
     public float Repulsion { get; set; }
     public float Range { get; set; }
-
     public String ImageName { get; set; }
-
 }
